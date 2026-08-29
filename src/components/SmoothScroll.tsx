3677df25b374eef5b0ea5ad9f62116eb0781e729
Varad Madhav <varadmadhav09@gmail.com>
2026-08-30 02:23:44 +0530

    added dynamic scroll to top reset on all route changes

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -2,11 +2,22 @@
 
 import React, { useEffect, useRef } from "react";
 import { ReactLenis } from "lenis/react";
+import { useLocation } from "react-router-dom";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   const lenisRef = useRef<any>(null);
+  const { pathname } = useLocation();
+
+  // Scroll to top automatically on route changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const lenisInstance = lenisRef.current?.lenis;
+    if (lenisInstance) {
+      lenisInstance.scrollTo(0, { immediate: true });
+    }
+  }, [pathname]);
 
   useEffect(() => {
     // Register GSAP ScrollTrigger plugin
